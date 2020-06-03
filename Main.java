002cce7d01aaef78d02bb11a80378b5cbf39dc0e
JasonLi1217 <66272048+JasonLi1217@users.noreply.github.com>
2020-06-03 11:52:20 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,31 +3,40 @@ Neo Navin, Jason Li
 ICS3U7
 Final Project: FlappyBird
 */
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-
+import java.io.*;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
 
 class FlappyBird implements ActionListener {
 	
 	public static FlappyBird flappybird;
 	
-	public final int WIDTH = 800, HEIGHT = 800;
+	public static final int WIDTH = 600, HEIGHT = 900;
 	public MyPanel panel;
+	public Bird bird;
+	public final BufferedImage img = ImageIO.read(new File("background.png"));
 	
-
-	public FlappyBird(){
+	public FlappyBird()throws IOException{
+		//background image
+		//JLabel background = new JLabel("", img, JLabel.CENTER);
+		//background.setBounds(0,0, WIDTH, HEIGHT);
+		
 		JFrame frame = new JFrame("FlappyBird");
 		panel = new MyPanel();
 		Timer timer = new Timer(20, this);
 		
+		//frame.add(background);
 		frame.add(panel);
 		frame.pack();
 		frame.setSize(WIDTH, HEIGHT);
 		frame.setResizable(false);
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		bird = new Bird(30);
 		
 		timer.start();
 
@@ -36,11 +45,17 @@ class FlappyBird implements ActionListener {
 		panel.repaint();
 	}
 	public void repaint(Graphics g){
-		System.out.print("Heloo ");
+		g.drawImage(img, 0, 0, null);
+		g.setColor(Color.ORANGE);
+		g.fillRect(bird.X()-bird.size(), bird.Y(), bird.size(), bird.size());
 	}
-	public static void main(String[] args) {
+	
+	
+	public static void main(String[] args) throws Exception{
 		flappybird = new FlappyBird();
 	}
 
   
 }
+
+
