0948a5fb29dcff581bdc3dd35beaff8bcececce8
Neo Navin <51327798+NavinNeo@users.noreply.github.com>
2020-06-03 21:52:21 -0400

    Update FlappyBird.java

--- FlappyBird.java
+++ FlappyBird.java
@@ -10,7 +10,7 @@ import java.io.*;
 import java.awt.image.BufferedImage;
 import javax.imageio.ImageIO;
 
-class FlappyBird implements ActionListener {
+class FlappyBird implements KeyListener, ActionListener {
 	
 	public static FlappyBird flappybird;
 	
@@ -36,6 +36,7 @@ class FlappyBird implements ActionListener {
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
+		frame.addKeyListener(this);
 		bird = new Bird(30);
 		
 		timer.start();
@@ -44,8 +45,29 @@ class FlappyBird implements ActionListener {
 	public void actionPerformed(ActionEvent e){
 		panel.repaint();
 	}
+		public void actionPerformed(ActionEvent e){
+		panel.repaint();
+	}
+	public void keyPressed( KeyEvent e )   {
+		if(e.getKeyCode()==32){
+			bird.jump(); //when pressed bird jumps v_y
+			bird.jumping = true;
+		}
+	}
+
+	public void keyReleased( KeyEvent e ){
+		if(e.getKeyCode()==32){
+			bird.jump(); 
+			bird.jumping = false; //bird can return to falling when released
+		}
+	}
+	public void keyTyped( KeyEvent e )  {}
 	public void repaint(Graphics g){
 		g.drawImage(img, 0, 0, null);
+		
+		if(bird.jumping == false){
+			bird.fall(); //bird falls when nothing happens
+		}
 		g.setColor(Color.ORANGE);
 		g.fillRect(bird.X()-bird.size(), bird.Y(), bird.size(), bird.size());
 	}
