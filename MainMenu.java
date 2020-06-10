263122cf12b51f98cf1977d5bcc553e0d91bd089
Neo Navin <51327798+NavinNeo@users.noreply.github.com>
2020-06-10 16:06:49 -0400

    Create MainMenu.java

---
+++ MainMenu.java
@@ -0,0 +1,52 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+
+class MainMenu extends JPanel implements ActionListener{
+	
+	public Timer timer;
+	private JButton play, shop, help;
+	
+	public MainMenu(){
+		timer = new Timer(20, this);
+		setLayout(new FlowLayout(FlowLayout.CENTER));
+		
+		//load icons
+		play = new JButton(loadAndResizeImg("play.png",190, 57));
+		
+		add(play);
+		play.addActionListener(this);
+		
+		timer.start();
+	}
+	public void actionPerformed(ActionEvent e){
+		if(e.getSource() == play){
+			Game.card.show(Game.c, "FlappyBird");
+			timer.stop();
+			FlappyBird.timer.start();
+		}
+		if(e.getSource() == timer){
+			repaint();
+		}
+	}
+	public void paintComponent(Graphics g){
+		super.paintComponent(g);
+		
+		g.drawImage(FlappyBird.background, 0, -200, null);
+		g.drawImage(FlappyBird.background, 760, -200, null);
+	}
+	
+	//this method allows to resize out icons for the buttons
+	public ImageIcon loadAndResizeImg(String path, int width, int height){
+		ImageIcon img = new ImageIcon(path);
+		Image image = img.getImage();
+		Image modifiedImage = image.getScaledInstance(width, height, java.awt.Image.SCALE_SMOOTH);
+		
+		img = new ImageIcon(modifiedImage);
+		return (img);
+	}
+	
+}
