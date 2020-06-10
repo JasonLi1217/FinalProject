7b3197154d3f117cd9599ed2d8a8a409f041157b
Neo Navin <51327798+NavinNeo@users.noreply.github.com>
2020-06-10 14:08:20 -0400

    Update FlappyBird.java

--- FlappyBird.java
+++ FlappyBird.java
@@ -1,129 +1,39 @@
-/*
-Neo Navin, Jason Li
-ICS3U7
-Final Project: FlappyBird
-*/
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.io.*;
 import java.awt.image.BufferedImage;
 import javax.imageio.ImageIO;
-import java.util.ArrayList;
-import java.util.Random;
 
+class FlappyBird extends JPanel implements KeyListener, ActionListener{
+	
+	public Image background;
+	static Timer timer;
+	
+	public FlappyBird() {
+		//load image
+		ImageIcon img = new ImageIcon ("background.png");
+		background = img.getImage();
 
-class FlappyBird implements KeyListener, ActionListener{
+		timer = new Timer(20, this);
 	
-	public static FlappyBird flappybird;
-	
-	public static final int WIDTH = 1300, HEIGHT = 700;
-	public static int ticks, birdSpeed;
-	
-	public MyPanel panel;
-	public Bird bird;
-	public ArrayList<Column> columns;
-	public Random rand;
-
-	
-	public final BufferedImage img = ImageIO.read(new File("background.png"));
-	
-	public FlappyBird()throws IOException{
+		addKeyListener(this);
 		
-		JFrame frame = new JFrame("FlappyBird");
-		panel = new MyPanel();
-		Timer timer = new Timer(20, this);
-		
-		rand = new Random();
-
-		frame.add(panel);
-		frame.pack();
-		frame.setSize(WIDTH, HEIGHT);
-		frame.setResizable(false);
-		frame.setVisible(true);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		frame.addKeyListener(this);
-		bird = new Bird(25);
-		columns = new ArrayList<Column>();
-		
-		for (int i=0; i<100; i++){
-			addColumn(true);
-		}
-		
-		
-		timer.start();
-
-	} 
+		//timer.start(); //dont start timer in constructor with cardLayout. Do it when this card is shown
+	}
 	public void actionPerformed(ActionEvent e){
-		//bird
-		ticks++;
+		repaint();
+	}
+	public void paintComponent(Graphics g){
+		super.paintComponent(g);
 		
-		if(ticks%2==0 && birdSpeed<15){
-			birdSpeed += 2;
-		}
-		
-		bird.y += birdSpeed;
-		//columns
-		int speed = 10;
-		
-		for (int i = 0; i < columns.size(); i++){
-			Column column = columns.get(i);
-
-			column.x -= speed;
-		}
-		
-		panel.repaint();
+		g.drawImage(background, 0, -200, null);
+		g.drawImage(background, 760, -200, null);		
 	}
 	
-	public void keyPressed( KeyEvent e )   {
-	}
-
-	public void keyReleased( KeyEvent e ){
-		if(e.getKeyCode()==32){
-			bird.jump(); 
-		}
-	}
-	public void keyTyped( KeyEvent e )  {}
-	
-	public void repaint(Graphics g){
-		g.drawImage(img, 0, -200, null);
-		g.drawImage(img, 760, -200, null);
-
-		g.setColor(Color.ORANGE);
-		g.fillRect(bird.X()-bird.size(), bird.Y(), bird.size(), bird.size());
-		
-		for (Column column: columns)
-		{
-			paintColumn(g, column);
-		}
-		
-	}
-	
-	public void addColumn(boolean start){
-		int gap = 200;
-		int width = 100;
-		int height = rand.nextInt(350)+50;
-		
-		if (start){
-			columns.add(new Column(WIDTH + width +columns.size()*300, HEIGHT-height, width, height));
-			columns.add(new Column(WIDTH + width +(columns.size()-1)*300,0, width, HEIGHT-height-gap));
-		}else{
-			columns.add(new Column(columns.get(columns.size() - 1).x + 600, HEIGHT - height, width, height));
-			columns.add(new Column(columns.get(columns.size() - 1).x, 0, width, HEIGHT - height - gap));
-		}
-			
-	}
-	
-	public void paintColumn(Graphics g, Column column){
-		g.setColor(Color.GREEN.darker());
-		g.fillRect(column.x, column.y, column.width, column.height);
-	}
+	public void keyPressed( KeyEvent e )   {}
+	public void keyReleased( KeyEvent e ){}
+	public void keyTyped( KeyEvent e )  {}	
 	
 	
-	public static void main(String[] args) throws Exception{
-		flappybird = new FlappyBird();
-	}
-
-  
 }
