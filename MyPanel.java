079bc2106f7e74cec9e350f6bad2700a4b00fe78
Neo Navin <51327798+NavinNeo@users.noreply.github.com>
2020-06-10 14:07:11 -0400

    Game.java

--- MyPanel.java
+++ MyPanel.java
@@ -1,12 +1,41 @@
+/*
+Neo Navin, Jason Li
+ICS3U7
+Final Project: FlappyBird
+*/
+//main class
 import javax.swing.*;
 import java.awt.*;
 
+class Game extends JFrame{
 
-class MyPanel extends JPanel {
+	static Container c;
+	static CardLayout card;
 	
-	public void paintComponent(Graphics g){
-		super.paintComponent(g);
-		FlappyBird.flappybird.repaint(g);
+	public Game(){
+		c = getContentPane();
+		card = new CardLayout();
+		c.setLayout(card);
+		
+		//MainMenu menu = new MainMenu();
+		FlappyBird flappy = new FlappyBird();
+		
+		//c.add("menu", menu);
+		c.add("FlappyBird", flappy);
+		
+	}
+	
+	public static void main(String[] args){
+		Game game = new Game();
+		game.setTitle("FlappyBird 2.0");
+		game.setSize(1350, 700);
+		game.setVisible(true);
+		game.setResizable(false);
+		game.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+		FlappyBird.timer.start();
+		card.show(c, "FlappyBird");
+		
 	}
 
 }
